8fa5af5df05556ac9a8b2400dd729fe75e31fcb8
ClemlG <legoues.c@gmail.com>
2022-02-03 18:29:25 +0100

    bonjour

---
+++ front/js/functions.js
@@ -0,0 +1,11 @@
+/**
+ * Vérifie si la réponse est ok et retourne le résultat de la méthode JSON de l'objet "Response"
+ *
+ * @param response
+ * @returns {any | Promise<any>}
+ */
+function handleResponse(response){
+    if(response.ok){
+        return response.json()
+    }
+}
---
+++ front/js/index.js
@@ -0,0 +1,33 @@
+// Déclaration
+
+/**
+ * Récuperation des données produits auprès de l'API et injection des élements dans la page d'accueil
+ */
+function fetchProductsAndInject() {
+    fetch("http://localhost:3000/api/products")
+        .then(handleResponse)
+        .then(function(products) {
+            // Récupération
+            const itemsEl = document.querySelector('section#items')
+
+            // Logique
+            for(let i = 0; i < products.length; i++) {
+                const product = products[i]
+                itemsEL.innerHTML = itemsEL.innerHTML + `
+<a href="">
+   <article>
+       <img src="${product.imageUrl}" alt="${product.altTxt}">
+       <h3 class="productName">${product.name}</h3>
+       <p class="productDescription">${product.description}</p>
+   </article>
+</a>`
+            }
+        })
+        .catch((err) => {
+
+        })
+}
+
+
+// Execution
+fetchProductsAndInject()
