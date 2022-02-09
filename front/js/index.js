12212fba98b46d60100faea36c3d63c6ae255d11
Clément Le Goues <klaamzer@macbook-pro-de-clement.home>
2022-02-09 13:14:00 +0100

    enable data in localStorage

--- front/js/index.js
+++ front/js/index.js
@@ -3,27 +3,26 @@
 /**
  * Récuperation des données produits auprès de l'API et injection des élements dans la page d'accueil
  */
-function fetchProductsAndInject() {
+function fetchProductsAndInject(){
     fetch("http://localhost:3000/api/products")
         .then(handleResponse)
-        .then(function(products) {
-            // Récupération
-            const itemsEl = document.querySelector('section#items')
-
-            // Logique
-            for(let i = 0; i < products.length; i++) {
+        //Réception de la réponse en JSON. Maintenant nous devons récuperer l'élement qui va comporter les produits et ensuite les afficher à l'intérieur
+        .then(function (products) {
+            console.log(products)
+            for(let i = 0; i < products.length; i++){
                 const product = products[i]
-                itemsEl.innerHTML = itemsEl.innerHTML + `
-<a href="">
-   <article>
-       <img src="${product.imageUrl}" alt="${product.altTxt}">
-       <h3 class="productName">${product.name}</h3>
-       <p class="productDescription">${product.description}</p>
-   </article>
-</a>`
+                const itemsEl = document.querySelector("section#items")
+                   itemsEl.innerHTML = itemsEl.innerHTML + `
+                    <a href="./product.html?id=${product._id}">
+                        <article>
+                            <img src="${product.imageUrl}" alt="${product.altTxt}">
+                            <h3 class="productName">${product.name}</h3>
+                            <p class="productDescription">${product.description}</p>
+                        </article>
+                    </a>`
             }
         })
-        .catch((err) => {
+        .catch(function(err) {
 
         })
 }
---
+++ front/js/product.js
@@ -0,0 +1,74 @@
+let urlString = window.location.href
+let url = new URL(urlString)
+let id = url.searchParams.get('id')
+
+// On vérifie si l'ID compris dans l'URL est valide
+if(id === null || id === "") {
+    throw 'URL Invalide'
+}
+
+// On veut récupérer au près de l'api les informations du produit dont l'id correspond à l'id qu'on a trouvé dans l'url
+
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then(handleResponse)
+    .then(function(product) {
+        console.log(product)
+        const itemImg = document.querySelector('article div.item__img')
+        itemImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+        const itemTitle = document.getElementById('title')
+        itemTitle.innerHTML = `<h1 id="title">${product.name}</h1>`
+        const itemPrice = document.querySelector('div.item__content__titlePrice > p span#price')
+        itemPrice.innerHTML = `<span id="price">${product.price}</span>`
+        const itemDesc = document.getElementById('description')
+        itemDesc.innerHTML = `<p id="description">${product.description}</p>`
+        console.log(product.colors[0]);
+        const itemSelect = document.getElementById('colors')
+        for(let i = 0; i < product.colors.length; i++){
+            console.log('On rajoute les couleurs: ' + product.colors);
+            itemSelect.innerHTML += `<option value="${product.colors[i]}">${product.colors[i]}</option>` 
+        }
+    })
+    .catch(function(err) {
+            
+    })
+
+    // ON VEUT QUE NOTRE PANIER SOIT UN TABLEAU QUI STOCK A LA FOIS L'ID DU PRODUIT/ LA QUANTITE DU PRODUIT/ LA COULEUR DU PRODUIT
+
+//On écoute le click de l'utilisateur sur le bouton 'ajouter au panier'
+//On execute un callback qui vient recuperer les données choisit par l'utilisateur
+//On stock les données dans le local storage 
+
+
+const buttonAddItem = document.getElementById('addToCart')
+buttonAddItem.addEventListener('click', function() {
+    //RECUPERATION 
+    let itemColors = document.getElementById('colors')
+    let selectedColor = itemColors.value
+    console.log(selectedColor)
+    let itemQty = document.getElementById('quantity')
+    let qty = parseInt(itemQty.value)
+    console.log(qty);
+
+    //VERIFICATION
+    if(selectedColor === null || selectedColor === "" || qty === 0) {
+        return
+    }
+
+    //ON STOCK LES DONNEES DANS LE LOCAL STORAGE
+    //RECUPERATION
+    const key = id + selectedColor
+    const keyValueQty = localStorage.getItem(key)  
+    console.log(keyValueQty);
+    
+    //VERIFICATION
+    //SI LA CLE EST VIDE ON AJOUTE LA QUANTITE INITIALE SAISIE PAR L UTILISATEUR SINON ON AJOUTE SUR LA QUANTITE EXISTANTE
+    if(!keyValueQty) {
+        localStorage.setItem(key, qty)
+    } else {
+        const newQtyAdded = parseInt(keyValueQty) + qty
+        //ON REMPLACE LA VALEUR EXISTANTE PAR LA NOUVELLE SUR LA MEME CLE
+        localStorage.setItem(key, newQtyAdded)
+    }
+})
+
+
